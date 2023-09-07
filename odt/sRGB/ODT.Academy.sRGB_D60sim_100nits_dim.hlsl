ac31022825754283984c1d3da9154672e60222c1
Santiago Velasquez <28SVELASQUEZ@go.dsdmail.net>
2023-09-07 09:32:45 -0600

    stuff

--- odt/sRGB/ODT.Academy.sRGB_D60sim_100nits_dim.hlsl
+++ odt/sRGB/ODT.Academy.sRGB_D60sim_100nits_dim.hlsl
@@ -1,8 +1,8 @@
-// <ACEStransformID>urn:ampas:aces:transformId:v1.5:ODT.Academy.RGBmonitor_100nits_dim.a1.0.3</ACEStransformID>
-// <ACESuserName>ACES 1.0 Output - sRGB</ACESuserName>
+// <ACEStransformID>urn:ampas:aces:transformId:v1.5:ODT.Academy.RGBmonitor_D60si_100nits_dim.a1.0.3</ACEStransformID>
+// <ACESuserName>ACES 1.0 Output - sRGB (D60 sim.)</ACESuserName>
 
 // 
-// Output Device Transform - RGB computer monitor
+// Output Device Transform - RGB computer monitor (D60 simulation)
 //
 
 //
@@ -13,7 +13,7 @@
 //  monitor for which this transform is designed does not exactly match the 
 //  specifications in IEC 61966-2-1:1999.
 // 
-//  The assumed observer adapted white is D65, and the viewing environment is 
+//  The assumed observer adapted white is D60, and the viewing environment is 
 //  that of a dim surround. 
 //
 //  The monitor specified is intended to be more typical of those found in 
@@ -37,7 +37,7 @@
 //
 // Assumed observer adapted white point:
 //         CIE 1931 chromaticities:    x            y
-//                                     0.3127       0.329
+//                                     0.32168      0.33767
 //
 // Viewing Environment:
 //   This ODT has a compensation for viewing environment variables more typical 
@@ -59,6 +59,8 @@ float3 ODT_sRGB( float3 oces)
   const float DISPGAMMA = 2.4;
   const float offset = 0.055;
 
+  const float SCALE = 0.955;
+
   // OCES to RGB rendering space
   float3 rgbPre = mult_f3_f33( oces, AP0_2_AP1_MAT);
 
@@ -74,6 +76,23 @@ float3 ODT_sRGB( float3 oces)
   linearCV.g = Y_2_linCV( rgbPost.g, CINEMA_WHITE, CINEMA_BLACK);
   linearCV.b = Y_2_linCV( rgbPost.b, CINEMA_WHITE, CINEMA_BLACK);
 
+  // --- Compensate for different white point being darker  --- //
+  // This adjustment corrects for an issue that exists in ODTs where the 
+  // device is calibrated to a white chromaticity other than that of the 
+  // adapted white.
+  // In order to produce D60 on a device calibrated to D65 white (i.e. 
+  // equal display code values yield CIE x,y chromaticities of 0.3217, 0.329) 
+  // the red channel is higher than blue and green to compensate for the 
+  // "bluer" D60 white. This is the intended behavior but it means that 
+  // without compensation, as highlights increase, the red channel will hit 
+  // the device maximum first and clip, resulting in a chromaticity shift as 
+  // the green and blue channels continue to increase.
+  // To avoid this clipping behavior, a slight scale factor is applied to 
+  // allow the ODT to simulate D60 within the D65 calibration white point. 
+
+  // Scale and clamp white to avoid casted highlights due to D60 simulation
+  linearCV = min( linearCV, 1.) * SCALE;
+
   // Apply gamma adjustment to compensate for dim surround
   linearCV = darkSurround_to_dimSurround( linearCV);
 
@@ -83,9 +102,6 @@ float3 ODT_sRGB( float3 oces)
   // Convert to display primary encoding
   // Rendering space RGB to XYZ
   float3 XYZ = mult_f3_f33( linearCV, AP1_2_XYZ_MAT);
-
-  // Apply CAT from ACES white point to assumed observer adapted white point
-  XYZ = mult_f3_f33( XYZ, D60_2_D65_CAT);
 
   // CIE XYZ to display primaries
   linearCV = mult_f3_f33( XYZ, XYZ_2_DISPLAY_PRI_MAT);
