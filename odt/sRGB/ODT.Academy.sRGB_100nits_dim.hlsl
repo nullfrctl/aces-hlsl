65d43804a8e39b287ccdeb72740089511e788445
Santiago Velasquez <28SVELASQUEZ@go.dsdmail.net>
2023-09-07 09:12:54 -0600

    outline sRGB ODT

---
+++ odt/sRGB/ODT.Academy.sRGB_100nits_dim.hlsl
@@ -0,0 +1,45 @@
+// <ACEStransformID>urn:ampas:aces:transformId:v1.5:ODT.Academy.RGBmonitor_100nits_dim.a1.0.3</ACEStransformID>
+// <ACESuserName>ACES 1.0 Output - sRGB</ACESuserName>
+
+// 
+// Output Device Transform - RGB computer monitor
+//
+
+//
+// Summary :
+//  This transform is intended for mapping OCES onto a desktop computer monitor 
+//  typical of those used in motion picture visual effects production. These 
+//  monitors may occasionally be referred to as "sRGB" displays, however, the 
+//  monitor for which this transform is designed does not exactly match the 
+//  specifications in IEC 61966-2-1:1999.
+// 
+//  The assumed observer adapted white is D65, and the viewing environment is 
+//  that of a dim surround. 
+//
+//  The monitor specified is intended to be more typical of those found in 
+//  visual effects production.
+//
+// Device Primaries : 
+//  Primaries are those specified in Rec. ITU-R BT.709
+//  CIE 1931 chromaticities:  x         y         Y
+//              Red:          0.64      0.33
+//              Green:        0.3       0.6
+//              Blue:         0.15      0.06
+//              White:        0.3127    0.329     100 cd/m^2
+//
+// Display EOTF :
+//  The reference electro-optical transfer function specified in 
+//  IEC 61966-2-1:1999.
+//  Note: This EOTF is *NOT* gamma 2.2
+//
+// Signal Range:
+//    This transform outputs full range code values.
+//
+// Assumed observer adapted white point:
+//         CIE 1931 chromaticities:    x            y
+//                                     0.3127       0.329
+//
+// Viewing Environment:
+//   This ODT has a compensation for viewing environment variables more typical 
+//   of those associated with video mastering.
+//
